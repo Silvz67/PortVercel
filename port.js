84ea0be97e6a260eb37a4d9854163a6c673a4381
silvz67 <silvaartr661@gmail.com>
2025-05-16 16:47:58 -0300

    Commit antes de upar no VERCEL

---
+++ port.js
@@ -0,0 +1,15 @@
+function enviarMensagem(event){
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5582996117038'
+
+    const texto = `Olá, meu nome é ${nome}, ${mensagem}`
+    const msgformatada = encodeURIComponent(texto)
+
+    const url = `https://wa.me/${telefone}?text=${msgformatada}`
+
+    console.log(url)
+    window.open(url, '_blank')
+}
